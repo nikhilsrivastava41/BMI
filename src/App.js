5915532f8d27a59917db494a532a9e4674dda092
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2020-06-30 15:28:25 +0530

    state update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>WEIGHT LOSS JOURNEY</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    o: false,
+    w: 1,
+    h: 1,
+    b: 1,
+  };
+  updatew = (e) => {
+    this.setState({
+      w: e.target.value,
+    });
+  };
+  updateh = (e) => {
+    this.setState({
+      h: e.target.value,
+    });
+  };
+  showbmi = () => {
+    this.setState((state) => ({
+      o: !state.o,
+      b: state.w / (state.h * state.h),
+    }));
+    console.log(this.state.b);
+  };
+  render() {
+    const on = this.state.o;
+    let f;
+    if (on) {
+      f = <h1>Your Bmi is {this.state.b}</h1>;
+    }
+    return (
+      <div className="App">
+        <h1>WEIGHT LOSS JOURNEY</h1>
+        <span>Enter Weight</span>
+        <div className="hey">
+          <input
+            type="text"
+            placeholder="weight (kgs)"
+            onChange={this.updatew}
+          />
+        </div>
+        <span>Enter Height</span>
+        <div className="hey">
+          <input type="text" placeholder="Height (m)" onChange={this.updateh} />
+        </div>
+        <button type="submit" onClick={this.showbmi}>
+          Submit
+        </button>
+        {f}
+      </div>
+    );
+  }
 }
 
 export default App;
